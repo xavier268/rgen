f299f9248120871025f4f7377db58000a4865de8
xavier@DellXPSA <xavier@DellXPSA>
2024-07-03 11:30:20 +0200

    v0.4.3

--- version.go
+++ version.go
@@ -1,8 +1,8 @@
 package rgen
 
-// v0.4.2.1	typos
+// v0.4.3	typos
 // v0.4.2	dedup added (bloom filter)
 // v0.4.1 	complete redesign from 0.3.x
-const VERSION = "0.4.2.1"
+const VERSION = "0.4.3"
 
 const COPYRIGHT = "(c) xavier gandillot 2022-2024"
