4ef43c9f00ca5a3f38ef4151f9600b410e8cd115
xavier@DellXPSA <xavier@DellXPSA>
2024-09-22 01:51:02 +0200

    v0.4.5

--- version.go
+++ version.go
@@ -1,9 +1,10 @@
 package rgen
 
+// v0.4.5   clean up, minor release
 // v0.4.4   added iterator API, bump to go 1.23, using task for convenience
 // v0.4.3	typos
 // v0.4.2	dedup added (bloom filter)
 // v0.4.1 	complete redesign from 0.3.x
-const VERSION = "0.4.4"
+const VERSION = "0.4.5"
 
 const COPYRIGHT = "(c) xavier gandillot 2022-2024"
